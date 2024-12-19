d0a040acf8c13d0e0f2bb16680f1138eb593a793
Adrian Ferreiro <adriferreiro.88@gmail.com>
2024-12-19 09:13:21 -0300

    Pantalla para cambiar colores

--- lib/presentation/providers/theme_provider.dart
+++ lib/presentation/providers/theme_provider.dart
@@ -1,3 +1,9 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:widgets_app/config/theme/app_theme.dart';
 
 final isDarkModeProvider = StateProvider((ref) => false);
+
+// Listado de colores inmutable
+final colorListProvider = Provider((ref) => colorList);
+// Provider lo crea "inmutable", no lo puedo cambiar pero vamos a poder acceder
+// a través del ref desde cualquier lado
