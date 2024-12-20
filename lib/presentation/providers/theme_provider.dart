1afec439383b9d0b3602a3cc1bc989f54bf830ad
Adrian Ferreiro <adriferreiro.88@gmail.com>
2024-12-20 10:13:34 -0300

    Riverpod - StateNotifier

--- lib/presentation/providers/theme_provider.dart
+++ lib/presentation/providers/theme_provider.dart
@@ -11,3 +11,37 @@ final isDarkModeProvider = StateProvider((ref) => false);
 
 // Un simple int
 final selectedColorProvider = StateProvider((ref) => 0);
+
+// Un objeto de tipo AppTheme (custom)
+// StateNotifierProvider<Null, Object?> primero el nombre de la clase que quiero que controle el estado
+// y el segundo el estado que vamos a  tener
+final themeNotifierProvider = StateNotifierProvider<ThemeNotifier, AppTheme>(
+  (ref) => ThemeNotifier(), //primera instancia de themenotifier
+);
+
+/*
+Resumen:
+  * Provider para valores inmutables
+  * StateProvider para mantener una pieza de estado
+  * StateNotifierProvider para mantener también un estado pero cuando el objeto
+  es un poco más elaborado
+ */
+
+class ThemeNotifier extends StateNotifier<AppTheme> {
+  //mantiene un estado en particular
+  //en este caso quiero mantener una instancia de mi apptheme
+  ThemeNotifier() : super(AppTheme());
+  //creo una instancia de apptheme con los valores por defecto
+  //si hacemos:
+  /*
+  void algo (){
+    state.  me habilita 
+    todo lo que existe dentro de appTheme
+    isDarkMode, selectedColor, etc. 
+  }
+   */
+
+  void toggleDarkMode() {}
+
+  void changeColorIndex(int colorIndex) {}
+}
